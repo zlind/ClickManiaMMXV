d983190cbcdf99d3a07122b31b54b57d95e4acf3
lindz0150 <lindz0150@FHC-IC0037269.wrdsb.ca>
2015-04-30 08:59:27 -0400

    test commit

--- SS1/src/Intro.java
+++ SS1/src/Intro.java
@@ -1,19 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+//Zach Lindner
 
-/**
- *
- * @author lindz0150
- */
 public class Intro {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         System.out.println("Welcome to ICS");
     }
 }
