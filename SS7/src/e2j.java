6bf43c5fd20307d7ae336551353cc148e0d7f7d9
lindz0150 <lindz0150@FHC-IC0037269.wrdsb.ca>
2015-04-30 09:32:12 -0400

    work on e2j

--- SS7/src/e2j.java
+++ SS7/src/e2j.java
@@ -5,9 +5,18 @@ import javax.swing.JOptionPane;
 public class e2j {
 
     public static void main(String[] args) {
-        
-        String sentence;
+
+        String sentence, split[];
+        StringBuilder sb;
         sentence = JOptionPane.showInputDialog("Sentence:");
         
+        split = sentence.split(" ");
+        sb = new StringBuilder();
+        for (int i = 0; i < split.length; i++) {
+            sb = new StringBuilder(split[i]);
+            sb.insert(i + 1, "ay");
+        }
+        sentence = sb.toString();
+        System.out.println(sentence);
     }
 }
