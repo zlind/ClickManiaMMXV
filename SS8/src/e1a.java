fa9faa049c2db25743c8094ea0c668b8b853f117
lindz0150 <lindz0150@FHC-IC0037269.wrdsb.ca>
2015-05-01 09:11:55 -0400

    e1a

--- SS8/src/e1a.java
+++ SS8/src/e1a.java
@@ -1,8 +1,23 @@
 //Zach Lindner
 
+import java.io.*;
+import java.util.*;
+
 public class e1a {
 
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws Exception {
+        double ardNum[] = new double[4], ardAvg[] = new double[4];
+        Scanner sin = new Scanner(new FileReader("81aIN.txt"));
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                ardNum[j] = sin.nextInt();
+                ardAvg[i] += ardNum[j];
+            }
+            ardAvg[i] /= ardAvg.length;
+            System.out.println("Average of Student #" + (i + 1) + ": " + ardAvg[i] + "%");
+        }
+
+        sin.close();
     }
 }
