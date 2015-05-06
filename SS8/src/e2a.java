1180d4b1329fe02ebc9fc023c4deb45ad059b4ab
zlind <zach.lindner@hotmail.com>
2015-05-06 19:12:48 -0400

    fixed e2a

--- SS8/src/e2a.java
+++ SS8/src/e2a.java
@@ -7,15 +7,11 @@ public class e2a {
 
     public static void main(String[] args) throws Exception {
         String sSentence, sSplit[];
-        int nLength = 0;
         Scanner fin = new Scanner(new FileReader("e2aIN.txt"));
         
         sSentence = fin.nextLine();
         sSplit = sSentence.split(" ");
-        for (int i = 0; i < sSplit.length; i++) {
-            nLength++;
-        }
-        System.out.println(nLength + " words");
+        System.out.println(sSplit.length + " words");
         fin.close();
     }
 }
