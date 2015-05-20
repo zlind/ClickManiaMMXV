fcf5ca4fe7d5951e0992af86014a6fdcf8adab86
lindz0150 <lindz0150@FHC-IC0037266.wrdsb.ca>
2015-05-20 13:41:01 -0400

    add FraMain + PanMain

---
+++ src/clickmaniammxv/FraMain.java
@@ -0,0 +1,7 @@
+package clickmaniammxv;
+
+import javax.swing.JFrame;
+
+public class FraMain extends JFrame {
+
+}
---
+++ src/clickmaniammxv/PanMain.java
@@ -0,0 +1,7 @@
+package clickmaniammxv;
+
+import javax.swing.JPanel;
+
+public class PanMain extends JPanel {
+    
+}
