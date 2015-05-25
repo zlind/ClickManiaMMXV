9b158a3e1dcdfb38ee2ce4a1f5f200014c1a12aa
lindz0150 <lindz0150@FHC-IC0037266.wrdsb.ca>
2015-05-25 14:30:19 -0400

    started on timers

--- src/clickmaniammxv/PanHud.java
+++ src/clickmaniammxv/PanHud.java
@@ -6,15 +6,13 @@ import javax.swing.JPanel;
 
 public class PanHud extends JPanel {
 
-    public static JLabel lblCash = new JLabel("$0"), lblPerSecond = new JLabel("$0 / second");
+    public static JLabel lblCash = new JLabel("$0", JLabel.LEFT), 
+            lblPerSecond = new JLabel("$0 / second", JLabel.RIGHT);
 
     public PanHud() {
         setLayout(new GridLayout(1, 2));
     
         add(lblCash);
         add(lblPerSecond);
-        
-        lblCash.setHorizontalAlignment(JLabel.LEFT);
-        lblPerSecond.setHorizontalAlignment(JLabel.RIGHT);
     }
 }
--- src/clickmaniammxv/PanStats.java
+++ src/clickmaniammxv/PanStats.java
@@ -1,16 +1,32 @@
 package clickmaniammxv;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class PanStats extends JPanel {
     
-    public static JLabel lblClicks = new JLabel("Clicks: 0", JLabel.CENTER);
+    public static JLabel lblClicks = new JLabel("Clicks: 0", JLabel.CENTER), 
+            lblTime = new JLabel("Total time played: 0s", JLabel.CENTER);
+    Timer tmrTime;
+    ActionListener time = new Time();
     
     public PanStats() {
-        setLayout(new GridLayout(1, 1));
+        tmrTime = new Timer(1, time);
+        setLayout(new GridLayout(2, 1));
         
         add(lblClicks);
+        add(lblTime);
+    }
+    
+    class Time implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            PanMain.panClick.setVisible(false);
+        }
     }
 }
