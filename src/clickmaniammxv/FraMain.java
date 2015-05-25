90d961592aba84ef9c3828386e55af38371f8db9
lindz0150 <lindz0150@FHC-IC0037266.wrdsb.ca>
2015-05-25 13:22:10 -0400

    added panstats, testing branches

--- src/clickmaniammxv/FraMain.java
+++ src/clickmaniammxv/FraMain.java
@@ -3,9 +3,11 @@ package clickmaniammxv;
 import javax.swing.JFrame;
 
 public class FraMain extends JFrame {
+    
+    public static PanMain panMain = new PanMain();
 
     public FraMain() {
-        add(new PanMain());
+        add(panMain);
         setTitle("Click Mania MMXV");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(900, 600);
