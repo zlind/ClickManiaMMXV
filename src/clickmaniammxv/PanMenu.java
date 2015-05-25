9c47cff4a7b962065d962a24ea4a36c44abe3282
websa2533 <websa2533@FHC-IC0037267.wrdsb.ca>
2015-05-25 14:30:41 -0400

    New Menu. options button for sound

--- src/clickmaniammxv/PanMenu.java
+++ src/clickmaniammxv/PanMenu.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 
 public class PanMenu extends JPanel {
 
-    JButton btnUpgrades = new JButton("Upgrades"), btnOptions = new JButton("Options"), btnStats = new JButton("Stats"), btnBack = new JButton("Back");
+    JButton btnUpgrades = new JButton("Upgrades"), btnOptions = new JButton("Options"), btnStats = new JButton("Stats"), btnBack = new JButton("Back"),btnSoundOn = new JButton ("On"),btnSoundOff = new JButton ("Off");
     ActionListener upgrades = new Upgrades(), options = new Options(), stats = new Stats(), back = new Back();
 
     public PanMenu() {
@@ -24,6 +24,7 @@ public class PanMenu extends JPanel {
         btnOptions.addActionListener(options);
         btnStats.addActionListener(stats);
         btnBack.addActionListener(back);
+        
     }
 
     class Upgrades implements ActionListener {
---
+++ src/clickmaniammxv/PanOption.java
@@ -0,0 +1,13 @@
+
+package clickmaniammxv;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+
+public class PanOption extends JPanel{
+    public PanOption() {
+        setLayout(new GridLayout(1,1));
+        
+    }
+}
