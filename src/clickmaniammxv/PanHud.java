97e63859061c51d4842ca5f8db78e0d07b8aea47
kavia2934 <kavia2934@FHC-IC0034106.wrdsb.ca>
2015-05-28 13:53:18 -0400

    Added $$ / click to PanHud

--- src/clickmaniammxv/PanHud.java
+++ src/clickmaniammxv/PanHud.java
@@ -6,17 +6,16 @@ import javax.swing.JPanel;
 
 public class PanHud extends JPanel {
 
-    public static JLabel lblCash = new JLabel("$0"), lblPerSecond = new JLabel("$0 / second")
-            , lblPerClick = new JLabel("$1 / click");
+    public static JLabel lblCash = new JLabel("$0"), lblPerSecond = new JLabel("$0 / second"), lblPerClick = new JLabel("$1 / click");
 
     public PanHud() {
         setLayout(new GridLayout(1, 2));
-    
+
         add(lblCash);
         add(lblPerSecond);
         add(lblPerClick);
-        
-        
+
+
         lblCash.setHorizontalAlignment(JLabel.LEFT);
         lblPerSecond.setHorizontalAlignment(JLabel.RIGHT);
         lblPerClick.setHorizontalAlignment(JLabel.RIGHT);
--- src/clickmaniammxv/PanStats.java
+++ src/clickmaniammxv/PanStats.java
@@ -5,12 +5,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PanStats extends JPanel {
-    
+
     public static JLabel lblClicks = new JLabel("Clicks: 0", JLabel.CENTER);
-    
+
     public PanStats() {
         setLayout(new GridLayout(1, 1));
-        
+
         add(lblClicks);
     }
 }
