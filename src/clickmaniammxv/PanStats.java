804bd2e32fc4abb30db627377f4031777a59cbe6
lindz0150 <lindz0150@FHC-IC0037266.wrdsb.ca>
2015-05-28 12:27:49 -0400

    :|

--- src/clickmaniammxv/PanStats.java
+++ src/clickmaniammxv/PanStats.java
@@ -5,23 +5,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
 public class PanStats extends JPanel {
-    
+
     public static JLabel lblClicks, lblCash, lblUpgrade1;
-    
+
     public PanStats() {
         setLayout(new GridLayout(1, 3));
-        
+
         lblClicks = new JLabel("Clicks: 0", JLabel.CENTER);
         lblCash = new JLabel("Total Cash Earned: $0", JLabel.CENTER);
         lblUpgrade1 = new JLabel("# of Upgrade 1: 0");
-        
+
         add(lblClicks);
-        add(lblTime);
     }
-    
+
     class Time implements ActionListener {
 
         @Override
