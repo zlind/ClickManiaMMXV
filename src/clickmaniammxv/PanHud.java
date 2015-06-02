47a8fee04e05223a293540627838a83887dca5bf
lindz0150 <lindz0150@FHC-IC0037266.wrdsb.ca>
2015-06-02 13:29:26 -0400

    :|

--- src/clickmaniammxv/PanHud.java
+++ src/clickmaniammxv/PanHud.java
@@ -6,18 +6,21 @@ import javax.swing.JPanel;
 
 public class PanHud extends JPanel {
 
-    public static JLabel lblCash, lblPerSecond;
+    public static JLabel lblCash, lblPerSecond, lblPerClick;
 
     public PanHud() {
         setLayout(new GridLayout(1, 2));
         
         lblCash = new JLabel("$0"); 
         lblPerSecond = new JLabel("$0 / second");
+        lblPerClick = new JLabel("$1 / click");
     
         add(lblCash);
         add(lblPerSecond);
+        add(lblPerClick);
         
         lblCash.setHorizontalAlignment(JLabel.LEFT);
-        lblPerSecond.setHorizontalAlignment(JLabel.RIGHT);
+        lblPerSecond.setHorizontalAlignment(JLabel.CENTER);
+        lblPerClick.setHorizontalAlignment(JLabel.RIGHT);
     }
 }
